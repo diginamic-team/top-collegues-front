42fc1e9d762a6a1042d97d80edc8e1eaf6f62c27
unknown <alex.ayato@gmail.com>
2019-03-26 12:40:44 +0100

    rebase fait

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Collegue } from './models';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'top-collegues-front';
+  title = 'bonjour-angular';
+  collegueDemo: Collegue = {
+    pseudo: 'Alex',
+    score: 700,
+    photoUrl: 'http://media.lelombrik.net/t/1fbf224e94372be315bd7e55dd67631c/p/01.jpg'
+  }
 }
