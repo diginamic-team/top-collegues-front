60700707a93c3d527d73d1ab44cdee931c8a98d6
unknown <alex.ayato@gmail.com>
2019-03-29 11:46:40 +0100

    bonne version prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@
 export const environment = {
   production: true,
   // Ajout du Backend
-  backendUrl: 'ec2-54-228-243-238.eu-west-1.compute.amazonaws.com/'
+  backendUrl: 'https://alexandre-top-collegues.herokuapp.com/'
 };
